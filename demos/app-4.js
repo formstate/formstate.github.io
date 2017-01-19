d45b21bb8a4fa55bf3ce575728a357fc8fb721d6
basarat <basarat@example.com>
2017-01-19 01:16:06 +0000

    [ci skip] deployment (2017-1-19-1-15)

--- demos/app-4.js
+++ demos/app-4.js
@@ -35849,49 +35849,20 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
-	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-	    return c > 3 && r && Object.defineProperty(target, key, r), r;
-	};
-	var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-	    return new (P || (P = Promise))(function (resolve, reject) {
-	        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-	        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-	        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-	        step((generator = generator.apply(thisArg, _arguments)).next());
-	    });
-	};
-	var __generator = (this && this.__generator) || function (thisArg, body) {
-	    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
-	    return { next: verb(0), "throw": verb(1), "return": verb(2) };
-	    function verb(n) { return function (v) { return step([n, v]); }; }
-	    function step(op) {
-	        if (f) throw new TypeError("Generator is already executing.");
-	        while (_) try {
-	            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-	            if (y = 0, t) op = [0, t.value];
-	            switch (op[0]) {
-	                case 0: case 1: t = op; break;
-	                case 4: _.label++; return { value: op[1], done: false };
-	                case 5: _.label++; y = op[1]; op = [0]; continue;
-	                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-	                default:
-	                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-	                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-	                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-	                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-	                    if (t[2]) _.ops.pop();
-	                    _.trys.pop(); continue;
-	            }
-	            op = body.call(thisArg, _);
-	        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-	        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-	    }
-	};
-	var mobx_1 = __webpack_require__(186);
-	var utils = __webpack_require__(364);
+	function __export(m) {
+	    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+	}
+	__export(__webpack_require__(364));
+	__export(__webpack_require__(365));
+	__export(__webpack_require__(367));
+	__export(__webpack_require__(368));
+
+
+/***/ },
+/* 364 */
+/***/ function(module, exports) {
+
+	"use strict";
 	/**
 	 * Runs the value through a list of validators. As soon as a validation error is detected, the error is returned
 	 */
@@ -35932,6 +35903,22 @@
 	    });
 	}
 	exports.applyValidators = applyValidators;
+
+
+/***/ },
+/* 365 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var mobx_1 = __webpack_require__(186);
+	var types_1 = __webpack_require__(364);
+	var utils_1 = __webpack_require__(366);
 	/**
 	 * Helps maintain the value + error information about a field
 	 *
@@ -35993,7 +35980,7 @@
 	            var lastValidationRequest = _this.lastValidationRequest;
 	            _this.validating = true;
 	            var value = _this.value;
-	            return applyValidators(_this.value, _this._validators || [])
+	            return types_1.applyValidators(_this.value, _this._validators || [])
 	                .then(mobx_1.action(function (fieldError) {
 	                if (_this.lastValidationRequest !== lastValidationRequest)
 	                    return;
@@ -36033,7 +36020,7 @@
 	         * Runs validation with debouncing to keep the UI super smoothly responsive
 	         * NOTE: also setup in constructor
 	         */
-	        this.queueValidation = utils.debounce(this.queuedValidationWakeup, 200);
+	        this.queueValidation = mobx_1.action(utils_1.debounce(this.queuedValidationWakeup, 200));
 	        this.onUpdate = function () {
 	            _this.config.onUpdate && _this.config.onUpdate(_this);
 	        };
@@ -36043,7 +36030,7 @@
 	            /**
 	             * Automatic validation configuration
 	             */
-	            _this.queueValidation = mobx_1.action(utils.debounce(_this.queuedValidationWakeup, config.autoValidationDebounceMs || 200));
+	            _this.queueValidation = mobx_1.action(utils_1.debounce(_this.queuedValidationWakeup, config.autoValidationDebounceMs || 200));
 	            _this.autoValidationEnabled = config.autoValidationEnabled == undefined ? true : config.autoValidationEnabled;
 	        });
 	    }
@@ -36103,11 +36090,103 @@
 	], FieldState.prototype, "queuedValidationWakeup", void 0);
 	__decorate([
 	    mobx_1.action
-	], FieldState.prototype, "queueValidation", void 0);
-	__decorate([
-	    mobx_1.action
 	], FieldState.prototype, "onUpdate", void 0);
 	exports.FieldState = FieldState;
+
+
+/***/ },
+/* 366 */
+/***/ function(module, exports) {
+
+	"use strict";
+	/**
+	 * Debounce
+	 */
+	var now = function () { return new Date().getTime(); };
+	function debounce(func, milliseconds, immediate) {
+	    if (immediate === void 0) { immediate = false; }
+	    var timeout, args, context, timestamp, result;
+	    var wait = milliseconds;
+	    var later = function () {
+	        var last = now() - timestamp;
+	        if (last < wait && last > 0) {
+	            timeout = setTimeout(later, wait - last);
+	        }
+	        else {
+	            timeout = null;
+	            if (!immediate) {
+	                result = func.apply(context, args);
+	                if (!timeout)
+	                    context = args = null;
+	            }
+	        }
+	    };
+	    return function () {
+	        context = this;
+	        args = arguments;
+	        timestamp = now();
+	        var callNow = immediate && !timeout;
+	        if (!timeout)
+	            timeout = setTimeout(later, wait);
+	        if (callNow) {
+	            result = func.apply(context, args);
+	            context = args = null;
+	        }
+	        return result;
+	    };
+	}
+	exports.debounce = debounce;
+	;
+
+
+/***/ },
+/* 367 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+	    return new (P || (P = Promise))(function (resolve, reject) {
+	        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+	        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+	        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+	        step((generator = generator.apply(thisArg, _arguments)).next());
+	    });
+	};
+	var __generator = (this && this.__generator) || function (thisArg, body) {
+	    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+	    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+	    function verb(n) { return function (v) { return step([n, v]); }; }
+	    function step(op) {
+	        if (f) throw new TypeError("Generator is already executing.");
+	        while (_) try {
+	            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+	            if (y = 0, t) op = [0, t.value];
+	            switch (op[0]) {
+	                case 0: case 1: t = op; break;
+	                case 4: _.label++; return { value: op[1], done: false };
+	                case 5: _.label++; y = op[1]; op = [0]; continue;
+	                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+	                default:
+	                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+	                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+	                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+	                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+	                    if (t[2]) _.ops.pop();
+	                    _.trys.pop(); continue;
+	            }
+	            op = body.call(thisArg, _);
+	        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+	        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+	    }
+	};
+	var mobx_1 = __webpack_require__(186);
+	var types_1 = __webpack_require__(364);
 	/**
 	 * Just a wrapper around the helpers for a set of FieldStates or FormStates
 	 */
@@ -36170,7 +36249,7 @@
 	                        });
 	                        if (done)
 	                            return [2 /*return*/, { hasError: true }];
-	                        return [4 /*yield*/, applyValidators(this.$, this._validators || [])];
+	                        return [4 /*yield*/, types_1.applyValidators(this.$, this._validators || [])];
 	                    case 2:
 	                        error = _a.sent();
 	                        res = mobx_1.runInAction(function () {
@@ -36234,6 +36313,20 @@
 	    mobx_1.computed
 	], FormState.prototype, "error", null);
 	exports.FormState = FormState;
+
+
+/***/ },
+/* 368 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+	    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+	    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+	    return c > 3 && r && Object.defineProperty(target, key, r), r;
+	};
+	var mobx_1 = __webpack_require__(186);
 	/**
 	 * Makes it easier to work with dynamically maintained array
 	 */
@@ -36299,51 +36392,6 @@
 	    mobx_1.computed
 	], FormStateLazy.prototype, "error", null);
 	exports.FormStateLazy = FormStateLazy;
-
-
-/***/ },
-/* 364 */
-/***/ function(module, exports) {
-
-	"use strict";
-	/**
-	 * Debounce
-	 */
-	var now = function () { return new Date().getTime(); };
-	function debounce(func, milliseconds, immediate) {
-	    if (immediate === void 0) { immediate = false; }
-	    var timeout, args, context, timestamp, result;
-	    var wait = milliseconds;
-	    var later = function () {
-	        var last = now() - timestamp;
-	        if (last < wait && last > 0) {
-	            timeout = setTimeout(later, wait - last);
-	        }
-	        else {
-	            timeout = null;
-	            if (!immediate) {
-	                result = func.apply(context, args);
-	                if (!timeout)
-	                    context = args = null;
-	            }
-	        }
-	    };
-	    return function () {
-	        context = this;
-	        args = arguments;
-	        timestamp = now();
-	        var callNow = immediate && !timeout;
-	        if (!timeout)
-	            timeout = setTimeout(later, wait);
-	        if (callNow) {
-	            result = func.apply(context, args);
-	            context = args = null;
-	        }
-	        return result;
-	    };
-	}
-	exports.debounce = debounce;
-	;
 
 
 /***/ }
