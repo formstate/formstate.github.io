c54e30d320a514ece50cc1e6ed63b3289104698a
basarat <basarat@example.com>
2017-01-19 00:03:37 +0000

    [ci skip] deployment (2017-1-19-0-3)

--- demos/app-3.js
+++ demos/app-3.js
@@ -35802,6 +35802,41 @@
 	    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
 	    return c > 3 && r && Object.defineProperty(target, key, r), r;
 	};
+	var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+	    return new (P || (P = Promise))(function (resolve, reject) {
+	        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+	        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+	        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+	        step((generator = generator.apply(thisArg, _arguments)).next());
+	    });
+	};
+	var __generator = (this && this.__generator) || function (thisArg, body) {
+	    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+	    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+	    function verb(n) { return function (v) { return step([n, v]); }; }
+	    function step(op) {
+	        if (f) throw new TypeError("Generator is already executing.");
+	        while (_) try {
+	            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+	            if (y = 0, t) op = [0, t.value];
+	            switch (op[0]) {
+	                case 0: case 1: t = op; break;
+	                case 4: _.label++; return { value: op[1], done: false };
+	                case 5: _.label++; y = op[1]; op = [0]; continue;
+	                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+	                default:
+	                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+	                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+	                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+	                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+	                    if (t[2]) _.ops.pop();
+	                    _.trys.pop(); continue;
+	            }
+	            op = body.call(thisArg, _);
+	        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+	        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+	    }
+	};
 	var mobx_1 = __webpack_require__(186);
 	var utils = __webpack_require__(364);
 	/**
@@ -36048,6 +36083,7 @@
 	            _this._validators = validators;
 	            return _this;
 	        };
+	        this._error = '';
 	        /**
 	         * Note:
 	         * - not use isArray as it might be an observable
@@ -36061,27 +36097,51 @@
 	     * - if no error also return the validated values against each key.
 	     */
 	    FormState.prototype.validate = function () {
-	        var _this = this;
-	        this.validating = true;
-	        var values = this.getValues();
-	        return Promise.all(values.map(function (value) { return value.validate(); }))
-	            .then(mobx_1.action(function (_) {
-	            _this.validating = false;
-	            var hasError = _this.hasError;
-	            if (hasError) {
-	                return { hasError: hasError };
-	            }
-	            else {
-	                return { hasError: hasError, value: _this.$ };
-	            }
-	        }));
+	        return __awaiter(this, void 0, void 0, function () {
+	            var _this = this;
+	            var values, fieldsResult, done, error, res;
+	            return __generator(this, function (_a) {
+	                switch (_a.label) {
+	                    case 0:
+	                        this.validating = true;
+	                        values = this.getValues();
+	                        return [4 /*yield*/, Promise.all(values.map(function (value) { return value.validate(); }))];
+	                    case 1:
+	                        fieldsResult = _a.sent();
+	                        done = mobx_1.runInAction(function () {
+	                            if (fieldsResult.some(function (f) { return f.hasError; })) {
+	                                _this.validating = false;
+	                                return true;
+	                            }
+	                            return false;
+	                        });
+	                        if (done)
+	                            return [2 /*return*/, { hasError: true }];
+	                        return [4 /*yield*/, applyValidators(this.$, this._validators || [])];
+	                    case 2:
+	                        error = _a.sent();
+	                        res = mobx_1.runInAction(function () {
+	                            if (error != _this._error) {
+	                                _this._error = error;
+	                            }
+	                            _this.validating = false;
+	                            var hasError = !!error;
+	                            if (hasError) {
+	                                return { hasError: true };
+	                            }
+	                            return { hasError: false, value: _this.$ };
+	                        });
+	                        return [2 /*return*/, res];
+	                }
+	            });
+	        });
 	    };
 	    Object.defineProperty(FormState.prototype, "hasError", {
 	        /**
 	         * Does any field have an error
 	         */
 	        get: function () {
-	            return this.getValues().some(function (f) { return f.hasError; });
+	            return this.getValues().some(function (f) { return f.hasError; }) || !!this._error;
 	        },
 	        enumerable: true,
 	        configurable: true
@@ -36092,7 +36152,7 @@
 	         */
 	        get: function () {
 	            var subItemWithError = this.getValues().find(function (f) { return !!f.hasError; });
-	            return subItemWithError.error;
+	            return subItemWithError ? subItemWithError.error : this._error;
 	        },
 	        enumerable: true,
 	        configurable: true
@@ -36111,6 +36171,9 @@
 	__decorate([
 	    mobx_1.action
 	], FormState.prototype, "validate", null);
+	__decorate([
+	    mobx_1.observable
+	], FormState.prototype, "_error", void 0);
 	__decorate([
 	    mobx_1.computed
 	], FormState.prototype, "hasError", null);
